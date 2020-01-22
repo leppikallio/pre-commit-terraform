3becfec7ecde83aeecc776826e087c3805debaef
leppikallio <petteri.leppikallio@nordcloud.com>
2020-01-22 22:52:51 +0100

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='pre-commit-terraform',
     description='Pre-commit hooks for terraform_docs_replace',
-    url='https://github.com/antonbabenko/pre-commit-terraform',
+    url='https://github.com/leppikallio/pre-commit-terraform',
     version_format='{tag}+{gitsha}',
 
     author='Contributors',
